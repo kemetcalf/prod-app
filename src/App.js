03d29a19ebbd9196d07782cbc140e6af5b0568bf
Kat <kemetcalf3.14@gmail.com>
2023-04-28 14:50:50 -0700

    Implements UploadForm functionality; adds input state to form fields; enables submit button to update items array state

--- src/App.js
+++ src/App.js
@@ -14,19 +14,36 @@ const photos = [
 ];
 
 function App() {
+	// 3.4: form state
+	const [input, setInput] = useState();
 	const [items, setItems] = useState(photos);
+	// 3.3: conditional rendering prop/fn
 	const [isCollapsed, collapse] = useState(false);
 
+	// 3.4: form state handlers
+	const handleOnChange = (e) => setInput(e.target.value);
+	const handleOnSubmit = (e) => {
+		e.preventDefault();
+		setItems([input, ...items]);
+	};
+	// 3.3: conditional rendering state toggler fn
 	const toggle = () => collapse(!isCollapsed);
 	return (
 		<>
 			<Navbar />
 			<div class="container text-center mt-5">
+				{/* 3.3: conditional rendering state toggler button */}
 				<button className="btn btn-success float-end" onClick={toggle}>
 					{isCollapsed ? "Close" : "+ Add"}
 				</button>
 				<div className="clearfix mb-4"></div>
-				<UploadForm isVisible={isCollapsed} />
+				<UploadForm
+					// 3.3: conditional rendering prop/fn
+					isVisible={isCollapsed}
+					// 3.4: form state handlers passed to form
+					onChange={handleOnChange}
+					onSubmit={handleOnSubmit}
+				/>
 				<h1>Gallery</h1>
 				<div className="row">
 					{items.map((photo) => (
--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,10 +1,17 @@
-const UploadForm = ({ isVisible }) => {
+const UploadForm = ({ isVisible, onChange, onSubmit }) => {
 	return (
+		// 3.3: conditional rendering prop/fn
+		// short circuit version; also works as ternary e.g. <condition> ? <renderthis> : <renderthat>/null
 		isVisible && (
 			<>
 				<p className="display-6 text-center mb-3">Upload Stock Image</p>
 				<div className="mb-5 d-flex align-items-center justify-content-center">
-					<form className="mb-2" style={{ textAlign: "left" }}>
+					<form
+						className="mb-2"
+						style={{ textAlign: "left" }}
+						// 3.4: sets form input as first item in items array, updates items array state
+						onSubmit={onSubmit}
+					>
 						<div className="mb-3">
 							<input
 								type="text"
@@ -12,10 +19,18 @@ const UploadForm = ({ isVisible }) => {
 								name="title"
 								placeholder="title"
 								aria-describedby="text"
+								// 3.4: sets form input state - link
+								onChange={onChange}
 							/>
 						</div>
 						<div className="mb-3">
-							<input type="file" className="form-control" name="file" />
+							<input
+								type="file"
+								className="form-control"
+								name="file"
+								// 3.4: sets form input state - file
+								onChange={onChange}
+							/>
 						</div>
 						<button type="submit" className="btn btn-success float-end">
 							Save changes
