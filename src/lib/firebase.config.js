957d615c1c914c72aa4d1b516f5e7db17ac656cf
Kat <kemetcalf3.14@gmail.com>
2023-05-02 13:05:15 -0700

    Connects Firebase and Firestore DB to Cloud Firestore

--- src/lib/firebase.config.js
+++ src/lib/firebase.config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_API_KEY,
@@ -13,4 +14,5 @@ const firebaseConfig = {
 // Initialize Firebase
 // test to check for required firebase config by looking for missing config obj or apiKey, returns firebase object initialized
 const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
 export const db = getFirestore(app);
