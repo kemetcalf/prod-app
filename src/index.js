0a26dd004a6e14d03f7fc8d23bfc5df46d7dba0c
Kat <kemetcalf3.14@gmail.com>
2023-05-08 21:19:16 -0700

    root Route defined

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -13,7 +13,9 @@ root.render(
 		<AuthProvider>
 			<Provider>
 				<Router>
-					<App />
+					<Routes>
+						<Route path="/" element={<App />} />
+					</Routes>
 				</Router>
 			</Provider>
 		</AuthProvider>
