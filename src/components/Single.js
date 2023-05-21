2d05593303410c08bcaade9b942db8ec2eafd36a
Kat <kemetcalf3.14@gmail.com>
2023-05-20 23:40:52 -0700

    Gives UpdateForm visibility state management

--- src/components/Single.js
+++ src/components/Single.js
@@ -1,26 +1,28 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useFirestoreContext } from "../context/FirestoreContext";
+import { useState } from "react";
 // import Firestore from "../handlers/firestore";
 import Card from "./Card";
 import UpdateForm from "./UpdateForm";
 
+function useToggle() {
+	const [formState, setFormState] = useState(false);
+	const toggle = () => {
+		setFormState(!formState);
+	};
+	return { formState, toggle };
+}
+
 const Single = () => {
 	const navigate = useNavigate();
-	const { dispatch, state } = useFirestoreContext();
-	const { isOpen: isVisible, inputs } = state;
+	const { state } = useFirestoreContext();
 	const { state: routerState } = useLocation();
+	const { formState, toggle } = useToggle();
+
 	// find() returns 1st obj in state.items arr that matches the given property (item) in this case by finding the item.id that matches the routerState.id returned by useLocation
 	const item = state.items.find((item) => item.id === routerState.id);
 
-	const toggle = (bool) => dispatch({ type: "collapse", payload: { bool } });
-
-	const backNav = () => {
-		if (isVisible) {
-			toggle(!isVisible);
-		}
-		navigate(-1);
-		return;
-	};
+	// const toggle = (bool) => dispatch({ type: "collapse", payload: { bool } });
 
 	// TODO: handleOnChange to setInputs
 	// TODO: new handleOnSubmit to updateDocs
@@ -29,23 +31,19 @@ const Single = () => {
 	// }
 	return (
 		<>
-			<button className="btn btn-link" onClick={backNav}>
+			<button className="btn btn-link" onClick={() => navigate(-1)}>
 				Back
 			</button>
 			<div className="d-flex justify-content-center mb-5">
 				<Card {...item} />
 			</div>
-			<button
-				type="button"
-				className="btn btn-warning"
-				onClick={() => toggle(!isVisible)}
-			>
+			<button type="button" className="btn btn-warning" onClick={toggle}>
 				Edit
 			</button>
 			<button type="button" className="btn btn-danger" onClick={() => {}}>
 				Delete
 			</button>
-			<UpdateForm inputs={inputs} isVisible={isVisible} />
+			{formState && <UpdateForm />}
 		</>
 	);
 };
--- src/components/UpdateForm.js
+++ src/components/UpdateForm.js
@@ -1,12 +1,14 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useFirestoreContext } from "../context/FirestoreContext";
 import Firestore from "../handlers/firestore";
 
 const { updateDoc } = Firestore;
 
 const UpdateForm = () => {
-	const { dispatch, state } = useFirestoreContext();
-	const { isOpen: isVisible, inputs } = state;
+	const {
+		dispatch,
+		state: { inputs },
+	} = useFirestoreContext();
 
 	const handleOnChange = (e) => {
 		dispatch({ type: "setInputs", payload: { value: e } });
@@ -21,37 +23,35 @@ const UpdateForm = () => {
 	}, [inputs]);
 
 	return (
-		isVisible && (
-			<>
-				<div className="mb-5 d-flex align-items-center justify-content-center">
-					<form
-						className="mb-2"
-						style={{ textAlign: "left" }}
-						// 3.4: sets form input as first item in items array, updates items array state
-						onSubmit={updateOnSubmit}
+		<>
+			<div className="mb-5 d-flex align-items-center justify-content-center">
+				<form
+					className="mb-2"
+					style={{ textAlign: "left" }}
+					// 3.4: sets form input as first item in items array, updates items array state
+					onSubmit={updateOnSubmit}
+				>
+					<div className="mb-3">
+						<input
+							type="text"
+							className="form-control"
+							name="title"
+							placeholder="title"
+							aria-describedby="text"
+							// 3.4: sets form input state - link
+							onChange={handleOnChange}
+						/>
+					</div>
+					<button
+						type="submit"
+						className="btn btn-success float-end"
+						disabled={isDisabled}
 					>
-						<div className="mb-3">
-							<input
-								type="text"
-								className="form-control"
-								name="title"
-								placeholder="title"
-								aria-describedby="text"
-								// 3.4: sets form input state - link
-								onChange={handleOnChange}
-							/>
-						</div>
-						<button
-							type="submit"
-							className="btn btn-success float-end"
-							disabled={isDisabled}
-						>
-							Save and upload
-						</button>
-					</form>
-				</div>
-			</>
-		)
+						Save and upload
+					</button>
+				</form>
+			</div>
+		</>
 	);
 };
 
